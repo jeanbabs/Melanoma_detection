f9ee0299e2f768c6308c890b84c9a009dbdf352c
Jean Babin <jean.babin@edu.ece.fr>
2019-01-26 13:20:00 +0100

    First commit

---
+++ submissions/starting_kit/classifier.py
@@ -0,0 +1,13 @@
+from sklearn import svm
+from sklearn.base import BaseEstimator
+
+class Classifier(BaseEstimator):
+    def __init__(self):
+        self.clf = svm.SVC()
+
+    def fit(self, X, y):
+        self.clf.fit(X, y)
+
+    def predict_proba(self, X):
+        y_pred_proba = self.clf.predict_proba(X)
+        return y_pred_proba
