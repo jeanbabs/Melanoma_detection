1876f06e459c345eeda79a51352217a1247ed197
Cyril9227 <cyril.equilbec@hotmail.fr>
2019-01-28 17:36:32 +0100

    Normalement c'est bon

--- download_data.py
+++ download_data.py
@@ -9,11 +9,14 @@ def main(dest_dir = 'data'):
         os.mkdir(dest_dir)
     output_file = os.path.join(dest_dir, "data.zip")
     
-    print("Starting download...")
-    gdd.download_file_from_google_drive(file_id='1z1uVQpQvtjJocgbANCxfFaEVGEzYhnOc', dest_path=output_file, unzip=True)
+    print("Starting downloading test data set...")
+    gdd.download_file_from_google_drive(file_id='1_Os49PsPTj3dX1i5XC9A8h6whbdNVG0S', dest_path=output_file, unzip=True)
+    print("=> File saved as {}".format(output_file))
+
+    print("Starting downloading train data set...")
+    gdd.download_file_from_google_drive(file_id='1Ai14wr8RwKdkFl3YJ55NfO00SqyZBpUN', dest_path=output_file, unzip=True)
     print("=> File saved as {}".format(output_file))
     
-
 
 
 if __name__ == '__main__':
