6416685ad89e0987db50f1d67b2ac2dd4e08a4e4
Jean Babin <jean.babin@edu.ece.fr>
2019-01-28 18:57:23 +0100

    Correction dl data

--- download_data.py
+++ download_data.py
@@ -7,14 +7,16 @@ from google_drive_downloader import GoogleDriveDownloader as gdd
 def main(dest_dir = 'data'):
     if not os.path.exists(dest_dir):
         os.mkdir(dest_dir)
-    output_file = os.path.join(dest_dir, "data.zip")
+    output_file = os.path.join(dest_dir, "data_test.zip")
     
     print("Starting downloading test data set...")
     gdd.download_file_from_google_drive(file_id='1_Os49PsPTj3dX1i5XC9A8h6whbdNVG0S', dest_path=output_file, unzip=True)
     print("=> File saved as {}".format(output_file))
 
+    output_file2 = os.path.join(dest_dir, "data_train.zip")
+
     print("Starting downloading train data set...")
-    gdd.download_file_from_google_drive(file_id='1Ai14wr8RwKdkFl3YJ55NfO00SqyZBpUN', dest_path=output_file, unzip=True)
+    gdd.download_file_from_google_drive(file_id='1Ai14wr8RwKdkFl3YJ55NfO00SqyZBpUN', dest_path=output_file2, unzip=True)
     print("=> File saved as {}".format(output_file))
     
 
