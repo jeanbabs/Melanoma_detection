22526355533c629cdbbbb9bc6cb786fcfe588033
Cyril9227 <cyril.equilbec@hotmail.fr>
2019-01-31 18:05:07 +0100

    2 baseline models

--- submissions/starting_kit/classifier.py
+++ submissions/starting_kit/classifier.py
@@ -1,9 +1,10 @@
 from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.base import BaseEstimator
 
 class Classifier(BaseEstimator):
     def __init__(self):
-        self.clf = svm.SVC(probability=True, gamma="auto")
+        self.clf = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
 
     def fit(self, X, y):
         self.clf.fit(X, y)
--- submissions/starting_kit/feature_extractor.py
+++ submissions/starting_kit/feature_extractor.py
@@ -1,10 +1,29 @@
+from skimage.transform import resize
+import numpy as np
+
+
+def crop_center(img, cropx, cropy):
+    y, x = img.shape
+    startx = x // 2 - (cropx // 2)
+    starty = y // 2 - (cropy // 2)    
+    return img[starty:starty + cropy, startx:startx + cropx]
+
+
 class FeatureExtractor():
     
     def __init__(self):
         pass
     
     def fit(self, X, y):
-        pass
+        return X
+        
+        
 
     def transform(self, X):
+        X = X.values
+        X = X.reshape(-1, 400, 400)
+        X = np.array([crop_center(image, 300, 300) for image in X])
+        X = X.reshape(-1, 300, 300, 1)
+        X = np.array([resize(image, (128, 128, 1), mode='reflect', anti_aliasing=True) for image in X])
+        X = X.reshape(-1, 128 * 128)
         return X
